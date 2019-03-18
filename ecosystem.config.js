6da4184ca12f37b005c6c3e6972d3834e5471388
Matt Chapman <chapabu101@gmail.com>
2019-03-18 09:47:12 +0000

    Add PM2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: 'dist/app.js',
+    instances: "max",
+    autorestart: true,
+    watch: 'dist/**/*.js',
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'app-watcher',
+    script: 'npm start',
+    instances: 1,
+    autorestart: true,
+    watch: 'tsconfig.json',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
